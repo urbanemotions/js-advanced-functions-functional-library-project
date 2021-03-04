331f03cdaace77b59870aa07e9c1a7a35a0321c9
sin wan wildy <sw.wildy@gmail.com>
2021-03-04 12:00:30 -0800

    commit tree c7b625306423a19c03af02f089b5954c0c68d939

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection, callback) {
+      for(let val in collection) {
+        callback(collection[val])
+      }
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection, callback) {
+      let newCollection = [];
+      for(let val in collection) {
+        newCollection.push(callback(collection[val]))
+      }
+      return newCollection;
     },
 
-    reduce: function() {
-
+    reduce: function(collection, callback, acc) {
+      // let result = acc;
+      for(let val in collection) {
+        let result = callback(val, acc)
+      }
+      return result;
     },
 
     functions: function() {
